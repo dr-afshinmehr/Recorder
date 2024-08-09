08f18017a9f3505900bc85c357adb6aedfd1f58c
Reza Afshinmehr <88842899+dr-afshinmehr@users.noreply.github.com>
2024-08-09 13:21:01 +0330

    add intial scale to register form

--- register.php
+++ register.php
@@ -7,6 +7,7 @@ $conn = connection_user();
 <html lang="fa">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ثبت نام در سایت</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/bootstrap.rtl.min.css">
